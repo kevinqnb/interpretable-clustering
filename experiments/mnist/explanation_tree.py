f2e6ffdbdec09cc929bd931735af1ee010d68ec1
Kevin Quinn <quinnk@scc1.bu.edu>
2025-03-13 09:50:23 -0400

    experiment results

--- experiments/mnist/explanation_tree.py
+++ experiments/mnist/explanation_tree.py
@@ -33,7 +33,7 @@ os.environ["OMP_NUM_THREADS"] = "1"
 
 #np.seterr(all='raise')
 prune_cpu_count = 1
-experiment_cpu_count = 16
+experiment_cpu_count = 12
 
 # REMINDER: The seed should only be initialized here. It should NOT 
 # within the parameters of any sub-function or class (except for select 
